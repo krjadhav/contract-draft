15f2b14ad068e719cf555dabf46967435e449b80
krjadhav <j@dhav.me>
2023-07-07 14:44:20 +0530

    Create models for response and db

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,6 @@
+from typing import List, Optional
+
+from fastapi import Depends
 from pydantic import BaseModel
 
 
@@ -7,3 +10,45 @@ class ContractDetails(BaseModel):
     party_a: str
     party_b: str
     effective_date: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = False
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+class Contract(BaseModel):
+    id: str
+    text: str
+
+
+class UserWithContracts(UserInDB):
+    contracts: Optional[List[Contract]] = []
+
+
+class ContractSave(BaseModel):
+    text: str
+
+
+class ContractResponse(BaseModel):
+    id: str
+    created_at: str
+
+
+class ContractClause(BaseModel):
+    text: str
